3e4c14e4ef84230c3f8c120a4208dfb7fb769423
HafidaAssendal <hafida.assendal@gmail.com>
2026-03-21 09:34:08 +0100

    chore(db): add SQL script to populate themes table

---
+++ sql/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO themes (title, description) VALUES
+('JavaScript', 'Langage de programmation incontournable du web, utilisé côté client et serveur avec Node.js.'),
+('Java', 'Langage orienté objet robuste et portable, très utilisé en entreprise et backend.'),
+('Python', 'Langage polyvalent idéal pour le data science, l intelligence artificielle et le scripting.'),
+('Angular', 'Framework frontend TypeScript développé par Google pour créer des applications web.');
