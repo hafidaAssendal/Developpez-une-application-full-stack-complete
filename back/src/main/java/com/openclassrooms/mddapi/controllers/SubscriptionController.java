47dbad86ad7c53b35dd4670b8861c8338fc116f7
HafidaAssendal <hafida.assendal@gmail.com>
2026-03-26 11:37:30 +0100

    feat(subscription): add SubscriptionController, SubscriptionService for subscribe endpoints

---
+++ back/src/main/java/com/openclassrooms/mddapi/controllers/SubscriptionController.java
@@ -0,0 +1,36 @@
+package com.openclassrooms.mddapi.controllers;
+
+import com.openclassrooms.mddapi.exception.BadRequestException;
+import com.openclassrooms.mddapi.exception.NotFoundException;
+import com.openclassrooms.mddapi.payload.response.MessageResponse;
+import com.openclassrooms.mddapi.services.SubscriptionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*", maxAge = 3400)
+@RestController
+@RequestMapping("api/subscriptions")
+public class SubscriptionController {
+  private final SubscriptionService subscriptionService;
+
+  public SubscriptionController(SubscriptionService subscriptionService) {
+    this.subscriptionService = subscriptionService;
+  }
+
+
+  // POST /api/subscriptions/{id_theme}
+  @PostMapping("/{id_theme}")
+  public ResponseEntity<MessageResponse> subscribe(Authentication authentication,
+                                                   @PathVariable Long id_theme) {
+    try {
+      subscriptionService.subscribe(authentication.getName(), id_theme);
+      return ResponseEntity.ok(new MessageResponse("Abonné!"));
+    } catch (NotFoundException e) {
+      throw new NotFoundException(e.getMessage());
+    } catch (BadRequestException e) {
+      throw new BadRequestException(e.getMessage());
+    }
+
+  }
+}
---
+++ back/src/main/java/com/openclassrooms/mddapi/services/SubscriptionService.java
@@ -0,0 +1,39 @@
+package com.openclassrooms.mddapi.services;
+
+import com.openclassrooms.mddapi.exception.BadRequestException;
+import com.openclassrooms.mddapi.exception.NotFoundException;
+import com.openclassrooms.mddapi.models.Subscription;
+import com.openclassrooms.mddapi.models.Theme;
+import com.openclassrooms.mddapi.models.User;
+import com.openclassrooms.mddapi.repository.SubscriptionRepository;
+import com.openclassrooms.mddapi.repository.ThemeRepository;
+import com.openclassrooms.mddapi.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SubscriptionService {
+  private final SubscriptionRepository subscriptionRepository;
+  private final UserRepository userRepository;
+  private final ThemeRepository themeRepository;
+
+  public SubscriptionService(SubscriptionRepository subscriptionRepository,
+                             UserRepository userRepository, ThemeRepository themeRepository) {
+    this.subscriptionRepository = subscriptionRepository;
+    this.userRepository = userRepository;
+    this.themeRepository = themeRepository;
+  }
+
+  public void subscribe(String mail, Long id_theme) {
+    User user = userRepository.findByEmail(mail).orElseThrow(() -> new NotFoundException("Utilisateur introuvable"));
+    Theme theme = themeRepository.findById(id_theme).orElseThrow(() -> new NotFoundException("Theme Introuvable"));
+    if (subscriptionRepository.existsByUserIdAndTheme(user.getId(), id_theme)) {
+      throw new BadRequestException("déja abonné à ce thème ");
+
+    }
+    Subscription subscription = new Subscription();
+    subscription.setUser(user);
+    subscription.setTheme(theme);
+    subscriptionRepository.save(subscription);
+
+  }
+}
