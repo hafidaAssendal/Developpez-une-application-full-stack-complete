6ab43a6fcc468f75e39c10ea57993fe4f2866a4c
HafidaAssendal <hafida.assendal@gmail.com>
2026-04-02 12:25:20 +0200

    refactor(front): migrate to standalone components

---
+++ front/src/app/app.routes.ts
@@ -0,0 +1,25 @@
+// src/app/app.routes.ts
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/home/home.component')
+      .then(m => m.HomeComponent)
+  },
+  // {
+  //   path: 'login',
+  //   loadComponent: () => import('./pages/auth/components/login/login.component')
+  //     .then(m => m.LoginComponent)
+  // },
+  // {
+  //   path: 'register',
+  //   loadComponent: () => import('./pages/auth/components/register/register.component')
+  //     .then(m => m.RegisterComponent)
+  // },
+  // {
+  //   path: '**',
+  //   loadComponent: () => import('./shared/components/not-found/not-found.component')
+  //     .then(m => m.NotFoundComponent)
+  // }
+];
