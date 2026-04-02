c72830ae8f2ac6e62b4e3f0706c7345bbeae381b
HafidaAssendal <hafida.assendal@gmail.com>
2026-04-02 19:53:02 +0200

    feat(app.routes): add login and register routes

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -7,19 +7,19 @@ export const routes: Routes = [
     loadComponent: () => import('./pages/home/home.component')
       .then(m => m.HomeComponent)
   },
-  // {
-  //   path: 'login',
-  //   loadComponent: () => import('./pages/auth/components/login/login.component')
-  //     .then(m => m.LoginComponent)
-  // },
-  // {
-  //   path: 'register',
-  //   loadComponent: () => import('./pages/auth/components/register/register.component')
-  //     .then(m => m.RegisterComponent)
-  // },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/auth/components/login/login.component')
+      .then(m => m.LoginComponent)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./pages/auth/components/register/register.component')
+      .then(m => m.RegisterComponent)
+  },
   {
     path: '**',
-    loadComponent: () => import('./shared/not-found/not-found.component')
+    loadComponent: () => import('./shared//not-found/not-found.component')
       .then(m => m.NotFoundComponent)
   }
 ];
