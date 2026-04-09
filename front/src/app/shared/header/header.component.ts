59571049020868de3297c7c963b16a99f10c4851
HafidaAssendal <hafida.assendal@gmail.com>
2026-04-09 15:25:36 +0200

    feat(header): add responsive navigation with mobile sidebar menu

--- front/src/app/shared/header/header.component.ts
+++ front/src/app/shared/header/header.component.ts
@@ -1,13 +1,36 @@
 import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { AuthService } from 'src/app/pages/auth/services/auth.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule
+  ],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  @Input() showNav: boolean = false;
+  @Input() showNav: boolean = true;
+   menuOpen = false;
+
+constructor( private authService:AuthService){
+
+}
+  logout(): void {
+ this.authService.logout();
+  this.menuOpen = false;
+  }
+
+
 }
